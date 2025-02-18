2791f7e59d0fde4da8309d82d44d882342584412
SuminJN <wjstnals1211@gmail.com>
2025-02-18 16:38:06 +0900

    init

---
+++ src/test/java/walab/nanuri/NanuriApplicationTests.java
@@ -0,0 +1,13 @@
+package walab.nanuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanuriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
